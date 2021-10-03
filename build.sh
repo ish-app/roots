bb49930caed4d636cc77008f9a77f2366acd73b8
Theodore Dubois <tblodt@icloud.com>
2021-10-03 14:46:59 -0700

    s/docker/podman

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 cd $(dirname "$0")
 
-docker build .
+podman build .
 
 roots=(appstore appstore-apk)
 mkdir -p out
 for root in "${roots[@]}"; do
-    image=$(docker images --filter label=ish.export="$root.tar.gz" --quiet | head -n1)
+    image=$(podman images --filter label=ish.export="$root.tar.gz" --quiet | head -n1)
     echo "exporting $root $image"
-    docker save "$image" -o out/"$root.raw.tar"
+    podman save "$image" -o out/"$root.raw.tar"
     ./flatten.py out/"$root.raw.tar" | gzip -n > out/"$root.tar.gz"
     rm out/"$root.raw.tar"
 done
