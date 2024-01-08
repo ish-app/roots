00712ff0a54b2839c5aa1a8ed758003ca65357dc
Theodore Dubois <tblodt@icloud.com>
2024-01-08 10:06:22 -0800

    Time for alpine 3.19

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
-FROM --platform=linux/386 docker.io/library/alpine:3.14.3 AS ish-alpine
+FROM --platform=linux/386 docker.io/library/alpine:3.19.0 AS ish-alpine
 
 FROM ish-alpine
 LABEL ish.export=appstore-apk.tar.gz
 RUN mkdir /ish && touch /ish/version
-RUN echo 296 > /ish/apk-version
+RUN echo 31900 > /ish/apk-version
 COPY apk-motd /etc/motd
--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 cd $(dirname "$0")
 
-podman build --platform=linux/386 .
+docker build --platform=linux/386 .
 
 roots=(appstore-apk)
 mkdir -p out
 for root in "${roots[@]}"; do
-    image=$(podman images --filter label=ish.export="$root.tar.gz" --quiet | head -n1)
+    image=$(docker images --filter label=ish.export="$root.tar.gz" --quiet | head -n1)
     echo "exporting $root $image"
-    podman save "$image" -o out/"$root.raw.tar"
+    docker save "$image" -o out/"$root.raw.tar"
     ./flatten.py out/"$root.raw.tar" | gzip -n > out/"$root.tar.gz"
     rm out/"$root.raw.tar"
 done
